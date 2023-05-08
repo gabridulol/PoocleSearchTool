1844815b9f88356bb046d2b7d0e3a847a7c81f2e
Kamagori <marcos.biscotto@ufv.br>
2023-05-08 17:47:57 -0300

    cabeçalho dos arquivos

--- headers/PATRICIA.h
+++ headers/PATRICIA.h
@@ -1,1 +1,6 @@
+// Gabriel Rodrigues Marques - 5097
+// Alan Araújo dos Reis - 5096
+// Marcos Biscotto de Oliveira - 4236
+// Luiz César Galvão Lima - 4216
+
 // Implementação de PATRICIA e suas operações
