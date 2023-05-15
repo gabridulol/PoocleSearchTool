f977c78000c9cb02ddffa670657b3c146e448a57
Gabriel Rodrigues Marques <gabriel.r.marques@ufv.br>
2023-05-15 02:58:22 -0300

    new commi: #include <gtk/gtk.h> ok

--- GTKProject/GTKInterface.h
+++ GTKProject/GTKInterface.h
@@ -3,7 +3,28 @@
 // Marcos Biscotto de Oliveira - 4236
 // Luiz César Galvão Lima - 4216
 
-// Implementação da interface gráfica via GTK e Glade
-
-#include "gtk/gtk.h"
-
+#include <gtk/gtk.h>
+// #include <gtj/gtk.h> -> include path
+// pkg-config --cflags gtk4
+// pkg-config --libs gtk4
+// "/usr/include/gtk-4.0",
+// "/usr/include/gio-unix-2.0",
+// "/usr/include/cairo",
+// "/usr/include/pango-1.0",
+// "/usr/include/harfbuzz",
+// "/usr/include/pango-1.0", 
+// "/usr/include/fribidi",
+// "/usr/include/harfbuzz",
+// "/usr/include/gdk-pixbuf-2.0",
+// "/usr/include/x86_64-linux-gnu",
+// "/usr/include/cairo",
+// "/usr/include/pixman-1", 
+// "/usr/include/uuid",
+// "/usr/include/freetype2", 
+// "/usr/include/libpng16",
+// "/usr/include/graphene-1.0",
+// "/usr/lib/x86_64-linux-gnu/graphene-1.0/include",
+// "/usr/include/libmount", 
+// "/usr/include/blkid",
+// "/usr/include/glib-2.0",
+// "/usr/lib/x86_64-linux-gnu/glib-2.0/include",
--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 // #include "./headers/poocleMenu.h"
 
 #include "./headers/poocleMenu.h"
