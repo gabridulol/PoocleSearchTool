319fc918bfbe33a961fa2bf87677f3cd8f08b100
lanzyn <alan.a.reis@ufv.br>
2023-05-19 21:35:47 -0300

    dad

--- main.c
+++ main.c
@@ -6,27 +6,6 @@
 #include "./headers/poocleMenu.h"
 
 int main() {
-    typePatPointer patTree;
-    typeDocList docList;
-    startDocList(&docList);
-    insertDocList(&docList, "arquivo1.txt", 1);
-    printDocList(docList);
-    startPatTree(&patTree);
-    patTree = insertPatTree(&patTree, docList, "palavra", 1);
-    patTree = insertPatTree(&patTree, docList, "passaro", 1);
-    patTree = insertPatTree(&patTree, docList, "amor", 1);
-    patTree = insertPatTree(&patTree, docList, "cristina", 1);
-    patTree = insertPatTree(&patTree, docList, "buceta", 1);
-    patTree = insertPatTree(&patTree, docList, "cristino", 1);
-    patTree = insertPatTree(&patTree, docList, "cristinu", 1);
-    patTree = insertPatTree(&patTree, docList, "cristinu", 1);
-    patTree = insertPatTree(&patTree, docList, "cristinu", 5);
-    patTree = insertPatTree(&patTree, docList, "amor", 1);   
-    patTree = insertPatTree(&patTree, docList, "passaro", 1);
-    patTree = insertPatTree(&patTree, docList, "passaram", 1);
-    patTree = insertPatTree(&patTree, docList, "passarao", 1);
-    patTree = insertPatTree(&patTree, docList, "passar", 1);
-    patTree = insertPatTree(&patTree, docList, "doug", 1);            
-    printPatTree(patTree);
+printf("buceta \n" "cu");
     return 0;
 }
