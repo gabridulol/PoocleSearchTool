c9d04673f1c21f803c66e6cf852e18c3a635b6db
Gabriel Rodrigues Marques <gabriel.r.marques@ufv.br>
2023-05-26 21:53:20 -0300

    new commit: GTK Work

--- headers/GTKInterface.h
+++ headers/GTKInterface.h
@@ -5,27 +5,14 @@
 
 #include "../headers/poocleMenu.h"
 
-// #include <gtj/gtk.h> -> include path
-// pkg-config --cflags gtk4
-// pkg-config --libs gtk4
-// "/usr/include/gtk-4.0",
-// "/usr/include/gio-unix-2.0",
-// "/usr/include/cairo",
-// "/usr/include/pango-1.0",
-// "/usr/include/harfbuzz",
-// "/usr/include/pango-1.0",
-// "/usr/include/fribidi",
-// "/usr/include/harfbuzz",
-// "/usr/include/gdk-pixbuf-2.0",
-// "/usr/include/x86_64-linux-gnu",
-// "/usr/include/cairo",
-// "/usr/include/pixman-1",
-// "/usr/include/uuid",
-// "/usr/include/freetype2",
-// "/usr/include/libpng16",
-// "/usr/include/graphene-1.0",
-// "/usr/lib/x86_64-linux-gnu/graphene-1.0/include",
-// "/usr/include/libmount",
-// "/usr/include/blkid",
-// "/usr/include/glib-2.0",
-// "/usr/lib/x86_64-linux-gnu/glib-2.0/include",
+typedef struct GTKData {
+    typePatPointer patTree;
+    typeDocList docList;
+} typeGTKData;
+
+void button_insert_source_clicked(GtkWidget *widget, gpointer data);
+void button_build_index_clicked(GtkWidget *widget, gpointer data);
+void button_print_index_clicked(GtkWidget *widget, gpointer data);
+void button_search_clicked(GtkWidget *widget, gpointer data);
+void button_exit_clicked(GtkWidget *widget, gpointer data);
+void gtkmain(int argc, char *argv[]);
--- sources/patTree.c
+++ sources/patTree.c
@@ -172,3 +172,55 @@ void printPatTree(typePatPointer patTree) {
         }
     }
 }
+
+void GTKPrintPatTree(typePatPointer patTree) {
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Print Pat Tree");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_container_set_border_width(GTK_CONTAINER(scrolled_window), 10);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+
+    GtkWidget *text_view = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+
+    if (patTree != NULL) {
+        GTKPrintPatTreeRecursive(patTree, buffer);
+    }
+
+    gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
+    gtk_container_add(GTK_CONTAINER(window), scrolled_window);
+    gtk_widget_show_all(window);
+
+    gtk_main();
+}
+
+void GTKPrintPatTreeRecursive(typePatPointer patTree, GtkTextBuffer *buffer) {
+    if (patTree != NULL) {
+        if (patTree->innext == inn) {
+            GTKPrintPatTreeRecursive(patTree->typeExtNode.typeInnNode.left, buffer);
+        }
+        if (patTree->innext == ext) {
+            gchar bufferText[256];
+            g_snprintf(bufferText, sizeof(bufferText), "%s ", patTree->typeExtNode.wordleData.wordChar);
+            gtk_text_buffer_insert_at_cursor(buffer, bufferText, -1);
+
+            typeIndexPointer auxIndex = patTree->typeExtNode.wordleData.indexList->firstCell->nextCell;
+            while (auxIndex != NULL) {
+                g_snprintf(bufferText, sizeof(bufferText), "<%d, %d> ", auxIndex->itemIndex.qtde, auxIndex->itemIndex.idDoc);
+                gtk_text_buffer_insert_at_cursor(buffer, bufferText, -1);
+                auxIndex = auxIndex->nextCell;
+            }
+
+            gtk_text_buffer_insert_at_cursor(buffer, "\n\n", -1);
+        }
+        if (patTree->innext == inn) {
+            GTKPrintPatTreeRecursive(patTree->typeExtNode.typeInnNode.right, buffer);
+        }
+    }
+}
